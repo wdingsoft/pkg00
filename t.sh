22a3ce4adeb1c764c37798e7b27d810cf0af9e67
weiding <weiding@weiding15s-mbp.attlocal.net>
2019-07-12 00:56:25 -0400

    t.sh

---
+++ t.sh
@@ -0,0 +1,15 @@
+#/bin/sh 
+
+
+
+git rm    "src/fo/kaogu/3meter_long_book_files/count.php"
+git rm    "src/fo/kaogu/3meter_long_book_files/pixel"
+git rm    "src/fo/minor_languages/huiZhu_languages_files/1L1.js"
+git rm    "src/fo/minor_languages/huiZhu_languages_files/73L1.js"
+git rm    "src/fo/minor_languages/huiZhu_languages_files/94L1.js"
+git rm    "src/pubimg/pictures/07/hao/__hao.txt"
+git rm    "src/pubimg/pictures/07/hao/hao_Virgin Mary with Jesus.txt"
+git rm    "src/pubimg/pictures/oorsc/ref/symbols/cross/iota-chi.txt"
+git rm    "src/pubimg/pictures/oorsc/ref/symbols/cross/maltese-alt1.txt"
+git rm    "src/pubimg/pictures/oorsc/ref/symbols/cross/maltese-alt8.txt"
+git rm    "src/pubimg/pictures/today/space2underscore.py"
